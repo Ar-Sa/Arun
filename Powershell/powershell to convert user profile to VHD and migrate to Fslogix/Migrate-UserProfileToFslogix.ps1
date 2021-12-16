b8bc91bcb5785822ad9b9481c45875120d8ea87a
Arun Sabale <toarun4u@gmail.com>
2021-12-16 11:39:55 -0500

    Create Migrate-UserProfileToFslogix.ps1

---
+++ Powershell/powershell to convert user profile to VHD and migrate to Fslogix/Migrate-UserProfileToFslogix.ps1
@@ -0,0 +1,37 @@
+<#
+.SYNOPSIS
+This script convert Local Profile to VHD and migrate to FSLogix Profile Container file share
+
+.NOTES
+  Version:          1.0
+  Author:           Arun Sabale          
+#>
+
+#########################################################################################
+# Requires -RunAsAdministrator
+# Requires FSLogix Agent with comes with FSlogix app(frx.exe)
+#########################################################################################
+# fslogix target file share profile path
+$FilesharePath = "<\\domain.com\share\path>"
+
+#########################################################################################
+# Do not edit here
+#########################################################################################
+$ENV:PATH=”$ENV:PATH;C:\Program Files\fslogix\apps\”
+$oldprofiles = gci c:\users | ?{$_.psiscontainer -eq $true} | select -Expand fullname | sort | out-gridview -OutputMode Multiple -title "Select profile(s) to convert"
+
+# foreach old profile
+foreach ($old in $oldprofiles) {
+
+$sam = ($old | split-path -leaf)
+$sid = (New-Object System.Security.Principal.NTAccount($sam)).translate([System.Security.Principal.SecurityIdentifier]).Value
+
+$nfolder = join-path $FilesharePath ($sam+"_"+$sid)
+if (!(test-path $nfolder)) {New-Item -Path $nfolder -ItemType directory | Out-Null}
+& icacls $nfolder /setowner "$env:userdomain\$sam" /T /C
+& icacls $nfolder /grant $env:userdomain\$sam`:`(OI`)`(CI`)F /T
+
+$vhd = Join-Path $nfolder ("Profile_"+$sam+".vhdx")
+
+frx.exe copy-profile -filename $vhd -sid $sid
+} 
